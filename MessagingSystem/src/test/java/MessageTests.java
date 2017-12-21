3238b04f51a29242621c2d159a117d47f16463b5
steve-attard <steve.attard.15@um.edu.mt>
2017-12-21 14:48:11 +0100

    Uploaded MessageTests

--- MessagingSystem/src/test/java/MessageTests.java
+++ MessagingSystem/src/test/java/MessageTests.java
@@ -1,32 +1,47 @@
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class MessageTests {
-//
-//    Message message;
-//    @Before
-//    public void setup() {
-//        message = new Message("src","trg",0,"msg");
-//    }
-//
-//    @After
-//    public void teardown() {
-//        message = null;
-//    }
-//
-//    @Test
-//    public void testOverloadedConstructor() {
-//
-//        setup();
-//
-//        //Verify
-//        assertEquals("src", message.sourceAgentID);
-//        assertEquals("trg", message.targetAgentID);
-//        assertEquals(0, message.timestamp);
-//        assertEquals("msg", message.messageContent);
-//        teardown();
-//    }
-//}
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MessageTests {
+
+    Message message;
+    @Before
+    public void setup() {
+        message = new Message("src","trg",0,"msg");
+    }
+
+    @After
+    public void teardown() {
+        message = null;
+    }
+
+    @Test
+    public void testGetSourceAgentID(){
+        setup();
+        assertEquals("src", message.getSourceAgentID());
+        teardown();
+    }
+
+    @Test
+    public void testGetTargetAgentID(){
+        setup();
+        assertEquals("trg", message.getTargetAgentID());
+        teardown();
+    }
+
+    @Test
+    public void testGetTimestamp(){
+        setup();
+        assertEquals(0, message.getTimestamp());
+        teardown();
+    }
+
+    @Test
+    public void testGetMessageContent(){
+        setup();
+        assertEquals("msg", message.getMessageContent());
+        teardown();
+    }
+}
